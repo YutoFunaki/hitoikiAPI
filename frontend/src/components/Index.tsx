d67d0f38875f0e2444ef00a886ff30f8b1253a21
YutoFunaki <kaizuka0629@gmail.com>
2025-05-06 21:06:03 +0900

    テストよう

--- frontend/src/components/Index.tsx
+++ frontend/src/components/Index.tsx
@@ -31,6 +31,7 @@ const Index: React.FC = () => {
                 setLoading(false);
             }
         };
+        console.log("VITE_API_BASE_URL:", import.meta.env.VITE_API_BASE_URL);
         fetchArticles();
     }, []);
 
